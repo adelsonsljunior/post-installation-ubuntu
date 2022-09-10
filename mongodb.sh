2ef3821520a675fd9f94677bcf756fa7ca2b15a6
adelsonsljunior <junioradelsonst@gmail.com>
2022-09-10 16:06:41 -0300

    corrigindo apt -y

--- mongodb.sh
+++ mongodb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo apt update
+sudo apt -y update
 
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
-sudo apt update
+sudo apt -y update
 
-sudo apt-get install -y mongodb-org
+sudo apt -y install mongodb-org
 
 sudo systemctl start mongod
 
