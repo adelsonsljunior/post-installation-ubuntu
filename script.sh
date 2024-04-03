d1b5a5127623417f906f271c089bd4b84b397f9a
adelsonsljunior <junioradelsonst@gmail.com>
2024-04-03 15:48:44 -0300

    feat: install git and change default branch

--- script.sh
+++ script.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+## Initial repository update
+
+sudo apt update -y
+
+## Install programs
+
+sudo apt install git -y
+
+## additional settings
+
+# Change git init default branch name
+
+git config --global init.defaultBranch main
