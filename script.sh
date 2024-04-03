bd1ad4eefd15d05cc809879538f7f9de9e24e0d5
adelsonsljunior <junioradelsonst@gmail.com>
2024-04-03 19:18:07 -0300

    feat: asdf, nodejs 20.12.1 and golang 1.22.1

--- script.sh
+++ script.sh
@@ -14,12 +14,27 @@ curl -fsSL https://get.docker.com | sudo bash
 
 sudo apt install neofetch tmux vim htop build-essential -y
 
+# SDKMAN
+
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 sdk install java
 sdk install quarkus
 sdk install kotlin
+
+# asdf
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+. "$HOME/.asdf/asdf.sh"
+
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf install nodejs 20.12.1
+asdf global nodejs 20.12.1
+
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+asdf install golang 1.22.1
+asdf global golang 1.22.1
 
 ## Additional settings
 
