4777be78760be9e73f85f93e1d16cba3546ab587
adelsonsljunior <junioradelsonst@gmail.com>
2024-04-05 12:30:29 -0300

    feat: vitualbox

--- script.sh
+++ script.sh
@@ -37,6 +37,24 @@ asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
 asdf install golang 1.22.1
 asdf global golang 1.22.1
 
+# VirtualBox
+
+cd /tmp
+
+curl https://www.virtualbox.org/download/oracle_vbox_2016.asc | gpg --dearmor > oracle_vbox_2016.gpg
+curl https://www.virtualbox.org/download/oracle_vbox.asc | gpg --dearmor > oracle_vbox.gpg
+sudo install -o root -g root -m 644 oracle_vbox_2016.gpg /etc/apt/trusted.gpg.d/
+sudo install -o root -g root -m 644 oracle_vbox.gpg /etc/apt/trusted.gpg.d/
+
+echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+
+sudo apt update -y
+sudo apt install -y linux-headers-$(uname -r) dkms
+sudo apt install virtualbox-7.0 -y
+
+wget https://download.virtualbox.org/virtualbox/7.0.0/Oracle_VM_VirtualBox_Extension_Pack-7.0.0.vbox-extpack
+sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-7.0.0.vbox-extpack
+
 ## Additional settings
 
 # Change git init default branch name
