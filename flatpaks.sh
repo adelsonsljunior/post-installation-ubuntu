34cee39a8188c0e08012ee51a4692144476e9750
adelsonsljunior <junioradelsonst@gmail.com>
2025-01-21 17:26:27 -0300

    flatpaks

---
+++ flatpaks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.rtosta.zapzap
+flatpak install flathub com.obsproject.Studio
+flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub org.localsend.localsend_app
+flatpak install flathub org.kde.okular
+flatpak install flathub org.telegram.desktop
